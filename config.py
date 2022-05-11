87a3c58a3706035d6fe6d1804ae66e7a04a3b35a
Eduardo Diaz <ediaz@dcc8090.cl>
2022-05-11 16:20:09 -0400

    actualiza para usar DB_USER en vez de USER

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-host = "db"
-database = "flask_db"
-user="user"
-password="pass"
+# config.py
+import os
+
+host = os.environ.get("HOST", default="db")
+database = os.environ.get("DATABASE", default="flask_db")
+user = os.environ.get("DB_USER", default="user")
+password = os.environ.get("DB_PASS", default="pass")
